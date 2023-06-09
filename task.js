531a7a65a042be6d4f342ddf0e8b1896b1c1b237
Pawan1208cse <125536864+Pawan1208cse@users.noreply.github.com>
2023-06-10 00:41:45 +0530

    Update task.js

--- task.js
+++ task.js
@@ -1,6 +1,6 @@
 function printNameAndAddress() {
-    var name = "Aaryan";
-    var address = "hawaiadda motihari";
+    var name = "pawan";
+    var address = "motihari";
   
     console.log("Name:", name);
     console.log("Address:", address);
